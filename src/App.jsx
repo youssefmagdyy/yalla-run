91c8b07a68a59630285711e722db9de00f51dd4e
youssefmagdyy <youssefmagdy1910@gmail.com>
2025-12-04 23:38:01 +0200

    no img downloads

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import "./App.css";
+import { useEffect } from "react";
 import Navbar from "./components/Navbar";
 import Community from "./sections/Community";
 import Footer from "./components/Footer";
@@ -8,6 +9,13 @@ import Hero from "./sections/Hero";
 import Gallery from "./sections/Gallery";
 
 export default function App() {
+
+  useEffect(() => {
+    const preventContext = e => e.preventDefault(); // block right-click
+    document.addEventListener("contextmenu", preventContext);
+
+    return () => document.removeEventListener("contextmenu", preventContext);
+  }, []);
 
   return (
     <div className="site">
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,4 +1,4 @@
-import { Fade, Slide } from "react-awesome-reveal";
+import { Fade } from "react-awesome-reveal";
 
 const About = () => {
     return (
